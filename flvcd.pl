430d6a9551f0f1f5e8f5294aedcc1f839e950fd2
Xeslaro <phoooyb@gmail.com>
2013-03-23 08:25:27 +0000

    add scripts for system start, reboot, shutdown, and modified youku.pl to more general ones using flvcd.com.

--- flvcd.pl
+++ flvcd.pl
@@ -1,16 +1,22 @@
 #!/usr/bin/perl -w
 use strict;
-for (@ARGV) {
-	while (s|:|%3A|) {};
-	while (s|/|%2F|) {};
-	$_ .= "&flag=one&format=super";
+use http;
+my ($f, $i) = ("", 0);
+while ($i < @ARGV) {
+	if ($ARGV[$i] eq "-f") {
+		$f = $ARGV[$i+1];
+		$i += 2;
+		next;
+	}
+	$_ = http::uri_escape($ARGV[$i++]);
+	$_ .= "&format=$f";
 	$_ = "www.flvcd.com/parse.php?kw=" . $_;
 	my @ans = `wget "$_" -O - | iconv -f gbk -t utf8`;
 	my $i = 0;
 	my ($name, $cnt);
 	while ($i <= $#ans) {
-		$name = $1 if ($ans[$i] =~ /^\s+(.*)\s+<strong>（请用右键"目标另存为"或硕鼠来快速下载.）<\/strong>/);
-		if ($ans[$i] =~ /由优酷网自动切割的<font color=red>(\d+)/) {
+		$name = $1 if ($ans[$i] =~ /^\s+(.*?)\s+<strong>/);
+		if ($ans[$i] =~ /自动切割的<font color=red>(\d+)/) {
 			$cnt = $1;
 			for (0..$cnt-1) {
 				$ans[$i + $_] =~ /href="(.*)" target=/;
