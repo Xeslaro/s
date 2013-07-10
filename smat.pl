3d906e44084c374cc95888f7c114519631ac338a
Xeslaro <phoooyb@gmail.com>
2013-07-10 04:55:50 +0000

    minor bug and formatting problem

--- smat.pl
+++ smat.pl
@@ -71,116 +71,120 @@ sub proc_unusual_courier {
 	for (values %iprice) {
 		$max_price = ($_ > $max_price) ? $_ : $max_price for (values %$_);
 	}
-	my @html = qx(wget -U chrome --header="Cookie: $search_acc_cookie" -O - "$referer" 2>/dev/null);
-	print $log "going to scanning for $referer\n" if ($debug);
-	my $cnt;
-	/searchResults_total">(.*?)</ && do {$cnt = $1; last} for (@html);
-	next unless (defined $cnt);
-	$cnt =~ s/,//g;
-	print $log "total item count is $cnt\n" if $debug;
-	loop: for (0..$cnt/10) {
-	      use integer;
-	      my $start = $_ * 10;
-	      my $count = ($_ == $cnt/10) ? $cnt%10 : 10;
-	      no integer;
-	      print $log "going to search for start=$start count=$count\n" if ($debug);
-	      last unless ($count);
-	      my $json = qx(wget -U chrome --header="Referer: $referer" --header="Cookie: $search_acc_cookie" -O - "$referer/render/?query=&start=$start&count=$count" 2>/dev/null);
-	      $debug && print($log "warning, json response failed, retrying\n"), redo unless $json =~ /^{"success":true/;
-	      $json =~ /total_count":(\d+)/;
-	      $debug && print($log "no listing for $referer now\n"), next unless defined $1 && $1 > 0;
-	      $json =~ /results_html":"(.*?)[^\\]"/;
-	      $debug && print($log "json response not valid, retrying\n"), redo unless defined $1;
-	      @html = split /\\n/, $1;
-	      (my $item_name) = ($json =~ /market_name":"(.*?)"/);
-	      $debug && print($log "referer name: $name\nitem: $item_name\ndon't match\n"), redo unless (defined $item_name && $name eq $item_name);
-	      my @descriptions = $json =~ /descriptions":\[(.*?)\]/g;
-	      my ($total, $subtotal, $description_cnt, $listing_id);
-	      $description_cnt = -1;
-	      $i = 0;
-	      while ($i < @html) {
-		      $_ = $html[$i++];
-		      if (/BuyMarketListing\('listing', '(.*?)'/) { $description_cnt++, $listing_id = $1; print $log "currenct listing id $listing_id\n" if ($debug); next; }
-		      if (/market_listing_price_with_fee/) {
-			      next unless $html[$i++] =~ /(\d+\.\d+)/; $total = $1*100;
-			      last loop if ($total > $max_price || $total > $wallet_balance);
-		      }
-		      if (/market_listing_price_without_fee/) {
-			      next unless $html[$i++] =~ /(\d+\.\d+)/; $subtotal = $1*100;
-			      my $fee = $total - $subtotal;
-			      my ($effect, $color);
-			      next loop unless defined $descriptions[$description_cnt];
-			      while ($descriptions[$description_cnt] =~ /value":"(.*?)"/g) {
-				      print $log "current description value is $1\n" if ($debug > 1);
-				      $_ = $1;
-				      if (/Effect: (.*)/) {
-					      print $log "effect is $1\n" if ($debug > 1);
-					      for (keys %effect_abbreviation_to_full) {
-						      $effect = $_ if ($1 eq $effect_abbreviation_to_full{$_});
-					      }
-				      }
-				      if (/Color: .*?(\d+, \d+, \d+)/) {
-					      $color = defined $usual_color{$1} ? $usual_color{$1} : "legacy";
-					      print $log "color is $color\n" if ($debug > 1);
-				      }
-			      }
-			      print $log "this item is of effect $effect & color $color & price $total\n" if ($debug && $effect);
-			      my $legacy = $color eq "legacy" && defined $iprice{legacy}{general};
-			      my $specific_effect = defined $effect && defined $iprice{$effect}{general};
-			      my $specific_effect_color = defined $effect && defined $iprice{$effect}{$color};
-			      if ($legacy || $specific_effect || $specific_effect_color) {
-				      my $highest_considered_price = 0;
-				      $highest_considered_price = $iprice{legacy}{general} if ($legacy && $iprice{legacy}{general} > $highest_considered_price);
-				      $highest_considered_price = $iprice{$effect}{general} if ($specific_effect && $iprice{$effect}{general} > $highest_considered_price);
-				      $highest_considered_price = $iprice{$effect}{$color} if ($specific_effect_color && $iprice{$effect}{$color} > $highest_considered_price);
-				      print $log "highest_considered_price is $highest_considered_price\n" if ($debug);
-				      next if ($total > $highest_considered_price);
-				      my $post_data = "sessionid=$sessionid&currency=1&subtotal=$subtotal&fee=$fee&total=$total";
-				      my $post_result = qx(wget -U chrome --header="Referer: $referer" --header="Cookie: $buying_acc_cookie" --post-data="$post_data" -O - "http://steamcommunity.com/market/buylisting/$listing_id" 2>/dev/null);
-				      print "effect: $effect color: $color referer: $referer\n";
-				      print "condition met, going to buy this item for $total\n";
-				      print "post_data is $post_data\n";
-				      unless ($?) {
-					      ($wallet_balance) = $post_result =~ /wallet_balance":(\d+)/;
-					      print "$post_result\nwallet_balance:$wallet_balance\n";
-				      }
-			      }
-		      }
-	      }
-      }
+	{
+		my @html = qx(wget -T 10 -U chrome --header="Cookie: $search_acc_cookie" -O - "$referer" 2>/dev/null);
+		print $log "going to scanning for $referer\n" if ($debug);
+		my $cnt;
+		/searchResults_total">(.*?)</ && do {$cnt = $1; last} for (@html);
+		next unless (defined $cnt);
+		$cnt =~ s/,//g;
+		print $log "total item count is $cnt\n" if $debug;
+		loop: for (0..$cnt/50) {
+			use integer;
+			my $start = $_ * 50;
+			my $count = ($_ == $cnt/50) ? $cnt%50 : 50;
+			no integer;
+			last unless ($count);
+			print $log "going to search for start=$start count=$count\n" if ($debug);
+			my $json = qx(wget -T 10 -U chrome --header="Referer: $referer" --header="Cookie: $search_acc_cookie" -O - "$referer/render/?query=&start=$start&count=$count" 2>/dev/null);
+			$debug && print($log "warning, json response failed, retrying\n"), redo unless $json =~ /^{"success":true/;
+			$json =~ /total_count":(\d+)/;
+			$debug && print($log "no listing for $referer now\n"), next unless defined $1 && $1 > 0;
+			$json =~ /results_html":"(.*?)[^\\]"/;
+			$debug && print($log "json response not valid, retrying\n"), redo unless defined $1;
+			@html = split /\\n/, $1;
+			(my $item_name) = ($json =~ /market_name":"(.*?)"/);
+			$debug && print($log "referer name: $name\nitem: $item_name\ndon't match\n"), redo unless (defined $item_name && $name eq $item_name);
+			my @descriptions = $json =~ /descriptions":\[(.*?)\]/g;
+			my ($total, $subtotal, $description_cnt, $listing_id);
+			$description_cnt = -1;
+			$i = 0;
+			while ($i < @html) {
+				$_ = $html[$i++];
+				if (/BuyMarketListing\('listing', '(.*?)'/) { $description_cnt++, $listing_id = $1; print $log "currenct listing id $listing_id\n" if ($debug); next; }
+				if (/market_listing_price_with_fee/) {
+					next unless $html[$i++] =~ /(\d+\.\d+)/; $total = $1*100;
+					last loop if ($total > $max_price || $total > $wallet_balance);
+				}
+				if (/market_listing_price_without_fee/) {
+					next unless $html[$i++] =~ /(\d+\.\d+)/; $subtotal = $1*100;
+					my $fee = $total - $subtotal;
+					my ($effect, $color);
+					next loop unless defined $descriptions[$description_cnt];
+					while ($descriptions[$description_cnt] =~ /value":"(.*?)"/g) {
+						print $log "current description value is $1\n" if ($debug > 1);
+						$_ = $1;
+						if (/Effect: (.*)/) {
+							print $log "effect is $1\n" if ($debug > 1);
+							for (keys %effect_abbreviation_to_full) {
+								$effect = $_ if ($1 eq $effect_abbreviation_to_full{$_});
+							}
+						}
+						if (/Color: .*?(\d+, \d+, \d+)/) {
+							$color = defined $usual_color{$1} ? $usual_color{$1} : "legacy";
+							print $log "color is $color\n" if ($debug > 1);
+						}
+					}
+					print $log "this item is of name $name & effect $effect & color $color & price $total\n" if ($debug && $effect);
+					my $legacy = $color eq "legacy" && defined $iprice{legacy}{general};
+					my $specific_effect = defined $effect && defined $iprice{$effect}{general};
+					my $specific_effect_color = defined $effect && defined $iprice{$effect}{$color};
+					if ($legacy || $specific_effect || $specific_effect_color) {
+						my $highest_considered_price = 0;
+						$highest_considered_price = $iprice{legacy}{general} if ($legacy && $iprice{legacy}{general} > $highest_considered_price);
+						$highest_considered_price = $iprice{$effect}{general} if ($specific_effect && $iprice{$effect}{general} > $highest_considered_price);
+						$highest_considered_price = $iprice{$effect}{$color} if ($specific_effect_color && $iprice{$effect}{$color} > $highest_considered_price);
+						print $log "highest_considered_price is $highest_considered_price\n" if ($debug);
+						next if ($total > $highest_considered_price);
+						my $post_data = "sessionid=$sessionid&currency=1&subtotal=$subtotal&fee=$fee&total=$total";
+						my $post_result = qx(wget -U chrome --header="Referer: $referer" --header="Cookie: $buying_acc_cookie" --post-data="$post_data" -O - "http://steamcommunity.com/market/buylisting/$listing_id" 2>/dev/null);
+						print "effect: $effect color: $color referer: $referer\n";
+						print "condition met, going to buy this item for $total\n";
+						print "post_data is $post_data\n";
+						unless ($?) {
+							($wallet_balance) = $post_result =~ /wallet_balance":(\d+)/;
+							print "$post_result\nwallet_balance:$wallet_balance\n";
+						}
+					}
+				}
+			}
+		}
+	}
 }
 sub proc_usual_item {
 	my ($referer, $iprice, $name) = @_;
 	my ($listing_id, $total, $subtotal);
-	my $json = qx(wget -U chrome --header="Referer: $referer" --header="Cookie: $search_acc_cookie" -O - "$referer/render/?query=&start=0&count=5" 2>/dev/null);
-	$debug && print($log "warning, json response failed, retrying\n"), redo unless $json =~ /^{"success":true/;
-	$json =~ /total_count":(\d+)/;
-	$debug && print($log "no listing for $referer now\n"), next unless defined $1 && $1 > 0;
-	$json =~ /results_html":"(.*?)[^\\]"/;
-	$debug && print($log "json response not valid, retrying\n"), redo unless defined $1;
-	my @html = split /\\n/, $1;
-	(my $item_name) = ($json =~ /market_name":"(.*?)"/);
-	$debug && print($log "referer name: $name\nitem: $item_name\ndon't match\n"), redo unless (defined $item_name && $name eq $item_name);
-	print $log "going to scanning price for $referer\n" if ($debug);
-	$i = 0;
-	while ($i < @html) {
-		$_ = $html[$i++];
-		if (/BuyMarketListing\('listing', '(.*?)'/) { $listing_id = $1; print $log "current listing id $listing_id\n" if ($debug); next; }
-		if (/market_listing_price_with_fee/) {
-			next unless $html[$i++] =~ /(\d+\.\d+)/; $total = $1*100;
-			print $log "current item price $total\n" if ($debug);
-			last if ($total > $iprice || $total > $wallet_balance);
-		}
-		if (/market_listing_price_without_fee/) {
-			next unless $html[$i++] =~ /(\d+\.\d+)/; $subtotal = $1*100;
-			my $fee = $total - $subtotal;
-			my $post_data = "sessionid=$sessionid&currency=1&subtotal=$subtotal&fee=$fee&total=$total";
-			my $post_result = qx(wget -U chrome --header="Referer: $referer" --header="Cookie: $buying_acc_cookie" --post-data="$post_data" -O - "http://steamcommunity.com/market/buylisting/$listing_id" 2>/dev/null);
-			print "$referer\ngoing to buy this item for $total, fee $fee, subtotal $subtotal\n";
-			print "post data is $post_data\n";
-			unless ($?) {
-				($wallet_balance) = $post_result =~ /wallet_balance":(\d+)/;
-				print "$post_result\nwallet_balance:$wallet_balance\n";
+	{
+		my $json = qx(wget -T 10 -U chrome --header="Referer: $referer" --header="Cookie: $search_acc_cookie" -O - "$referer/render/?query=&start=0&count=5" 2>/dev/null);
+		$debug && print($log "warning, json response failed, retrying\n"), redo unless $json =~ /^{"success":true/;
+		$json =~ /total_count":(\d+)/;
+		$debug && print($log "no listing for $referer now\n"), next unless defined $1 && $1 > 0;
+		$json =~ /results_html":"(.*?)[^\\]"/;
+		$debug && print($log "json response not valid, retrying\n"), redo unless defined $1;
+		my @html = split /\\n/, $1;
+		(my $item_name) = ($json =~ /market_name":"(.*?)"/);
+		$debug && print($log "referer name: $name\nitem: $item_name\ndon't match\n"), redo unless (defined $item_name && $name eq $item_name);
+		print $log "going to scanning price for $referer\n" if ($debug);
+		$i = 0;
+		while ($i < @html) {
+			$_ = $html[$i++];
+			if (/BuyMarketListing\('listing', '(.*?)'/) { $listing_id = $1; print $log "current listing id $listing_id\n" if ($debug); next; }
+			if (/market_listing_price_with_fee/) {
+				next unless $html[$i++] =~ /(\d+\.\d+)/; $total = $1*100;
+				print $log "current item price $total\n" if ($debug);
+				last if ($total > $iprice || $total > $wallet_balance);
+			}
+			if (/market_listing_price_without_fee/) {
+				next unless $html[$i++] =~ /(\d+\.\d+)/; $subtotal = $1*100;
+				my $fee = $total - $subtotal;
+				my $post_data = "sessionid=$sessionid&currency=1&subtotal=$subtotal&fee=$fee&total=$total";
+				my $post_result = qx(wget -U chrome --header="Referer: $referer" --header="Cookie: $buying_acc_cookie" --post-data="$post_data" -O - "http://steamcommunity.com/market/buylisting/$listing_id" 2>/dev/null);
+				print "$referer\ngoing to buy this item for $total, fee $fee, subtotal $subtotal\n";
+				print "post data is $post_data\n";
+				unless ($?) {
+					($wallet_balance) = $post_result =~ /wallet_balance":(\d+)/;
+					print "$post_result\nwallet_balance:$wallet_balance\n";
+				}
 			}
 		}
 	}
